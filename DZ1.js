f1bb430ed3cb43d0942ed1baafd0c0b0bef6bd4f
Поцылуенко <83078431+EvgenPotsulyenko@users.noreply.github.com>
2023-03-07 14:55:41 +0300

    Add files via upload

---
+++ DZ1.js
@@ -0,0 +1,14 @@
+// console.log("nodePath:");
+
+// function handler(obj)
+//     {   
+//         alert(obj.href);
+//             //const sql = `INSERT INTO users(name, age) VALUES('')`;
+//     }
+
+
+//     document.getElementById("btnCreate").onclick = handler;
+
+ var age = prompt("Введите свое имя:");
+ alert('Привет ' + age);
+
